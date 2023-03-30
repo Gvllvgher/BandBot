04627bcad42618f8f1912e8ea91ba3fd83e46f39
Justin Gallagher <justingallagher@outlook.com>
2023-03-29 21:50:20 -0400

    use environment variables for config

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import datetime
-
+from os import environ
 
 # hour=24, minute=0 would result in 12:00:00 AM
 # hour=10, minute=10 would result in 10:10:00 AM
 # hour=12, minute=30, second=50 would result in 12:30:50 PM
 
-DAY = "Monday" # Set this to whatever day you want it to run, Monday, Tuesday, etc...
-NOTIFY_CHANNEL = 0000 # The ID of the channel you want the message to be sent to
-TASK_DATE = datetime.time(hour=0, minute=0, second=0) # Timezone is UTC
-DATABASE = "postgresql://user:password@host/database" # PostgreSQL database connection URL
-BOT_TOKEN = "" # Your bot's token
+hour = int(environ['BOT_HOUR'])
+minute = int(environ['BOT_MINUTE'])
+second = int(environ['BOT_SECOND'])
+TASK_DATE = datetime.time(hour=hour, minute=minute, second=second) # Timezone is UTC
+DAY = environ['BOT_DAY'] # Set this to whatever day you want it to run, Monday, Tuesday, etc...
+NOTIFY_CHANNEL = int(environ['BOT_CHANNEL']) # The ID of the channel you want the message to be sent to
+DATABASE = environ['PG_CONNECT_STRING'] # "postgresql://user:password@host/database" # PostgreSQL database connection URL
+BOT_TOKEN = environ['BOT_TOKEN'] # Your bot's token
